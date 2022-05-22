9dfe8be0dc35fe7d07668a2c64d7cda7e77684e7
Labib2003 <labib.salimi30@gmail.com>
2022-05-22 11:18:56 +0600

    test commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/shared/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <p className='text-2xl'>Hello from tailwind</p>
+    </div>
+  );
+}
+
+export default App;

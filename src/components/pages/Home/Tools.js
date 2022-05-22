b682fd65a829cdf8aab50931147fdbe7501380af
Labib2003 <labib.salimi30@gmail.com>
2022-05-22 13:15:49 +0600

    tools data loaded form mongodb and local server

--- src/components/pages/Home/Tools.js
+++ src/components/pages/Home/Tools.js
@@ -4,7 +4,7 @@ import Tool from './Tool';
 
 const Tools = () => {
     const { isLoading, error, data: tools } = useQuery('tools', () =>
-        fetch('toolsFakeData.json').then(res =>
+        fetch('http://localhost:5000/tools').then(res =>
             res.json()
         )
     )
@@ -14,7 +14,7 @@ const Tools = () => {
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             {
-                tools.map(tool => <Tool key={tool.id} tool={tool}></Tool>)
+                tools.map(tool => <Tool key={tool._id} tool={tool}></Tool>)
             }
         </div>
     );
